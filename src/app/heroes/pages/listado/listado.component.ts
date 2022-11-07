6216f77b28c819101595956d59b4c886d054003a
Alonso <miguelretamosoin@gmail.com>
2022-11-07 02:09:01 -0500

    interfaz heroe :D

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { RESTHeroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -8,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: RESTHeroe[] = []
 
-  ngOnInit(): void {
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit(): void { //TODO llamamos metodo get luego de importar nuestro servicio :D
+
+    this.heroesService.getHeroes()//TODO obteniendo la respuesta del GET heroes :D
+      .subscribe(heroes => this.heroes = heroes) //TODO 
   }
 
 }

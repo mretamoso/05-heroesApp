4e38c9450ea0e5d44b10a1a784d30d99fa91adef
Alonso <miguelretamosoin@gmail.com>
2022-11-07 04:32:12 -0500

    heroe diseño

--- src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/heroes/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -4,11 +4,15 @@ import { RESTHeroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html',
-  styles: [
+  styles: [`
+  mat-card{
+    margin-top:20px;
+  }
+`
   ]
 })
 export class HeroeTarjetaComponent {
 
-  @Input() heroe!: RESTHeroe ; //TODO siempre se tendra
+  @Input() heroe!: RESTHeroe; //TODO siempre se tendra
 
 }
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -5,12 +5,7 @@ import { RESTHeroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [`
-    mat-card{
-      margin-top:20px;
-    }
-  `
-  ]
+  
 })
 export class ListadoComponent implements OnInit {
 

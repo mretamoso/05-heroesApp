34c2fa4f1ce06d433084d9ce7fe11823460fdba8
Alonso <miguelretamosoin@gmail.com>
2022-11-07 04:39:51 -0500

    variables de entorno

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RESTHeroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseurl
+
   constructor(private http: HttpClient) { } //TODO se crea http con httpclient ya importado previamente
 
   getHeroes(): Observable<RESTHeroe[]> { //TODO se indica que es de tipo observable de nombre heroe
-    return this.http.get<RESTHeroe[]>('http://localhost:3000/heroes')
+    return this.http.get<RESTHeroe[]>(`${this.baseUrl}/heroes`)
   }
 
   getHeroesPorId(id: string): Observable<RESTHeroe> { //TODO se indica que es de tipo observable de nombre heroe
-    return this.http.get<RESTHeroe>(`http://localhost:3000/heroes/${id}`)
+    return this.http.get<RESTHeroe>(`${this.baseUrl}/heroes/${id}`)
   }
 }

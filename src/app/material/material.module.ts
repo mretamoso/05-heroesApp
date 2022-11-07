261fbefab96daff99a56a5a62a9ca55b221e599f
Alonso <miguelretamosoin@gmail.com>
2022-11-07 00:55:21 -0500

    add mat nav,bot,icon,

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,8 +4,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
-
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -14,7 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }

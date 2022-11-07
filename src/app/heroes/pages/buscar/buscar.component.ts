d7d97f360f99965340aae7c06972fb56a38f7501
Alonso <miguelretamosoin@gmail.com>
2022-11-07 06:50:35 -0500

    autocompletado listo

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -13,7 +13,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: RESTHeroe[] = []
-  heroeSeleccionado!:RESTHeroe
+  heroeSeleccionado: RESTHeroe | undefined;
 
   constructor(private heroesService: HeroesService) { }
 
@@ -21,15 +21,21 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando() {
-    this.heroesService.getSugerencias(this.termino)
+    this.heroesService.getSugerencias(this.termino.trim())
       .subscribe(heroes => this.heroes = heroes)
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+
+    if (!event.option.value) {
+      this.heroeSeleccionado=undefined;
+      return;
+    }
+
     const heroe: RESTHeroe = event.option.value;
-    this.termino= heroe.superhero
+    this.termino = heroe.superhero;
 
     this.heroesService.getHeroesPorId(heroe.id!) //TODO siempre va a tener un valor 
-    .subscribe(heroe => this.heroeSeleccionado = heroe)
+      .subscribe(heroe => this.heroeSeleccionado = heroe)
   }
 }

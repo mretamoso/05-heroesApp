dede8698e9bbfd5e2b341b779acc433825117476
Alonso <miguelretamosoin@gmail.com>
2022-11-07 19:50:13 -0500

    add heroku api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseurl:'http://localhost:3000'
+  // baseurl:'http://localhost:3000'
+  baseurl:'https://json-heroes-mar.herokuapp.com'
 };
 
 /*
